94cc4c58edd6610b16aa88fad50710f338c904c4
barsu5489 <ebarsulai@gmail.com>
2023-03-16 12:54:58 +0300

    record validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  
+    validate :click_baity?
+  
+    def click_baity?
+      unless title&.include?("Won't Believe") || title&.include?("Secret") || title&.include?("Guess") || title&.match?(/Top \d+/)
+        errors.add(:title, 'must be clickbait-y')
+      end
+    end
+  end
+  
